028541a72aeea9d93239d736716ab9838dd607d5
nclozza <nclozza@itba.edu.ar>
2019-05-30 02:41:25 -0300

    Modified the main method to wrap basic functions

--- src/main/java/com/sia/tp3/App.java
+++ src/main/java/com/sia/tp3/App.java
@@ -1,24 +1,33 @@
 package com.sia.tp3;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         Configuracion configuracion = new Configuracion();
-        Multiplicador multiplicador = new Multiplicador(configuracion.getFuerza(), configuracion.getAgilidad(), configuracion.getPericia(), configuracion.getResistencia(), configuracion.getVida());
+        Multiplicador multiplicador = new Multiplicador(configuracion.getFuerza(), configuracion.getAgilidad(),
+                configuracion.getPericia(), configuracion.getResistencia(), configuracion.getVida());
         Poblacion poblacion = new Poblacion(configuracion.getPersonaje(), multiplicador);
 
-        poblacion.getPersonajes().get(0).imprimirGenes();
-        poblacion.getPersonajes().get(1).imprimirGenes();
-        System.out.println("ASD");
+        System.out.println("TERMINE DE CARGAR");
 
-        Cruce cruce = new Cruce();
-        cruce.enDosPuntos(poblacion.getPersonajes().get(0), poblacion.getPersonajes().get(1), 0, 2);
+//        for (int i = 0; i < poblacion.getPersonajes().size(); i++) {
+//            System.out.println("Personaje nro: " + i);
+//            System.out.println("Desempenio: " + poblacion.getPersonajes().get(i).getDesempenio());
+//            poblacion.getPersonajes().get(i).imprimirGenes();
+//        }
 
-        poblacion.getPersonajes().get(0).imprimirGenes();
-        poblacion.getPersonajes().get(1).imprimirGenes();
+        int aux = 0;
+        Reemplazo reemplazo = new Reemplazo(1, poblacion.getPersonajes().size());
 
-        System.out.println("ASD");
+        while (aux++ < configuracion.getGeneraciones()) {
+            poblacion.setPersonajes(reemplazo.obtenerNuevaGeneracion(poblacion.getPersonajes()));
+        }
+
+//        for (int i = 0; i < poblacion.getPersonajes().size(); i++) {
+//            System.out.println("Personaje nro: " + i);
+//            System.out.println("Desempenio: " + poblacion.getPersonajes().get(i).getDesempenio());
+//            poblacion.getPersonajes().get(i).imprimirGenes();
+//        }
+
 
     }
 }

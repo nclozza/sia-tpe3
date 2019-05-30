1e7bdc32ab304a798bf2705e1f3e77bf7a0332ea
nclozza <nclozza@itba.edu.ar>
2019-05-30 02:40:00 -0300

    Changed items load

--- src/main/java/com/sia/tp3/Poblacion.java
+++ src/main/java/com/sia/tp3/Poblacion.java
@@ -6,137 +6,161 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Poblacion {
 
-    private List<Personaje> personajes;
+    private ArrayList<Personaje> personajes;
     private Integer cantidadPersonajes;
+
+    private static final String path = "src/main/java/com/sia/tp3/fulldata/";
 
     public Poblacion(String personaje, Multiplicador multiplicador) {
 
-      List<Arma> armas = leerArmas();
-      List<Bota> botas = leerBotas();
-      List<Casco> cascos = leerCascos();
-      List<Guante> guantes = leerGuantes();
-      List<Pechera> pecheras = leerPecheras();
-      this.personajes = new LinkedList<>();
+        List<Arma> armas = leerArmas();
+        List<Bota> botas = leerBotas();
+        List<Casco> cascos = leerCascos();
+        List<Guante> guantes = leerGuantes();
+        List<Pechera> pecheras = leerPecheras();
+        cantidadPersonajes = armas.size();
+        personajes = new ArrayList<>();
 
-      for (int i=0; i < this.cantidadPersonajes; i++){
-        this.personajes.add(new Personaje(personaje, multiplicador, armas.get(i), botas.get(i), cascos.get(i), guantes.get(i), pecheras.get(i)));
-      }
+        Iterator iteratorArmas = armas.iterator();
+        Iterator iteratorBotas = botas.iterator();
+        Iterator iteratorCascos = cascos.iterator();
+        Iterator iteratorGuantes = guantes.iterator();
+        Iterator iteratorPecheras = pecheras.iterator();
+
+        for (int i = 0; i < cantidadPersonajes; i++) {
+            if ((i % 10000) == 0) {
+                System.out.println("FEDE ASI SE ITERA...");
+            }
+            personajes.add(new Personaje(personaje, multiplicador, (Arma) iteratorArmas.next(),
+                    (Bota) iteratorBotas.next(), (Casco) iteratorCascos.next(), (Guante) iteratorGuantes.next(),
+                    (Pechera) iteratorPecheras.next()));
+        }
+
+//        for (int i = 0; i < this.cantidadPersonajes; i++) {
+//            if ((i % 10000) == 0) {
+//                System.out.println("FEDE ASI SE ITERA...");
+//            }
+//            personajes.add(new Personaje(personaje, multiplicador, armas.get(i), botas.get(i), cascos.get(i),
+//                    guantes.get(i), pecheras.get(i)));
+//        }
     }
 
-    public List<Personaje> getPersonajes() {
+    public ArrayList<Personaje> getPersonajes() {
         return personajes;
     }
 
-    public void setPersonajes(List<Personaje> personajes) {
+    public void setPersonajes(ArrayList<Personaje> personajes) {
         this.personajes = personajes;
     }
 
-    public List<Arma> leerArmas (){
+    public List<Arma> leerArmas() {
         List<Arma> armas = new LinkedList<>();
         this.cantidadPersonajes = 0;
         try {
-            BufferedReader buffer = new BufferedReader(new FileReader("src/main/java/com/sia/tp3/testdata/armas.tsv"));
+            BufferedReader buffer = new BufferedReader(new FileReader(path + "armas.tsv"));
             String line;
             String[] datos;
             buffer.readLine();
 
-            while ((line = buffer.readLine()) != null){
+            while ((line = buffer.readLine()) != null) {
 
                 datos = line.split("\t");
-                armas.add(Integer.parseInt(datos[0]),  new Arma(Double.parseDouble(datos[1]),Double.parseDouble(datos[2]),Double.parseDouble(datos[3]),Double.parseDouble(datos[4]),Double.parseDouble(datos[5])));
+                armas.add(Integer.parseInt(datos[0]), new Arma(Double.parseDouble(datos[1]),
+                        Double.parseDouble(datos[2]), Double.parseDouble(datos[3]), Double.parseDouble(datos[4]),
+                        Double.parseDouble(datos[5])));
                 this.cantidadPersonajes++;
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return armas;
     }
 
-    public List<Bota> leerBotas (){
+    public List<Bota> leerBotas() {
         List<Bota> botas = new LinkedList<>();
         try {
-            BufferedReader buffer = new BufferedReader(new FileReader("src/main/java/com/sia/tp3/testdata/botas.tsv"));
+            BufferedReader buffer = new BufferedReader(new FileReader(path + "botas.tsv"));
             String line;
             String[] datos;
             buffer.readLine();
 
-            while ((line = buffer.readLine()) != null){
+            while ((line = buffer.readLine()) != null) {
 
                 datos = line.split("\t");
-                botas.add(Integer.parseInt(datos[0]),  new Bota(Double.parseDouble(datos[1]),Double.parseDouble(datos[2]),Double.parseDouble(datos[3]),Double.parseDouble(datos[4]),Double.parseDouble(datos[5])));
+                botas.add(Integer.parseInt(datos[0]), new Bota(Double.parseDouble(datos[1]),
+                        Double.parseDouble(datos[2]), Double.parseDouble(datos[3]), Double.parseDouble(datos[4]),
+                        Double.parseDouble(datos[5])));
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return botas;
     }
 
-    public List<Casco> leerCascos (){
+    public List<Casco> leerCascos() {
         List<Casco> cascos = new LinkedList<>();
         try {
-            BufferedReader buffer = new BufferedReader(new FileReader("src/main/java/com/sia/tp3/testdata/cascos.tsv"));
+            BufferedReader buffer = new BufferedReader(new FileReader(path + "cascos.tsv"));
             String line;
             String[] datos;
             buffer.readLine();
 
-            while ((line = buffer.readLine()) != null){
+            while ((line = buffer.readLine()) != null) {
 
                 datos = line.split("\t");
-                cascos.add(Integer.parseInt(datos[0]),  new Casco(Double.parseDouble(datos[1]),Double.parseDouble(datos[2]),Double.parseDouble(datos[3]),Double.parseDouble(datos[4]),Double.parseDouble(datos[5])));
+                cascos.add(Integer.parseInt(datos[0]), new Casco(Double.parseDouble(datos[1]),
+                        Double.parseDouble(datos[2]), Double.parseDouble(datos[3]), Double.parseDouble(datos[4]),
+                        Double.parseDouble(datos[5])));
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return cascos;
     }
 
-    public List<Guante> leerGuantes (){
+    public List<Guante> leerGuantes() {
         List<Guante> guantes = new LinkedList<>();
         try {
-            BufferedReader buffer = new BufferedReader(new FileReader("src/main/java/com/sia/tp3/testdata/guantes.tsv"));
+            BufferedReader buffer = new BufferedReader(new FileReader(path + "guantes.tsv"));
             String line;
             String[] datos;
             buffer.readLine();
 
-            while ((line = buffer.readLine()) != null){
+            while ((line = buffer.readLine()) != null) {
 
                 datos = line.split("\t");
-                guantes.add(Integer.parseInt(datos[0]),  new Guante(Double.parseDouble(datos[1]),Double.parseDouble(datos[2]),Double.parseDouble(datos[3]),Double.parseDouble(datos[4]),Double.parseDouble(datos[5])));
+                guantes.add(Integer.parseInt(datos[0]), new Guante(Double.parseDouble(datos[1]),
+                        Double.parseDouble(datos[2]), Double.parseDouble(datos[3]), Double.parseDouble(datos[4]),
+                        Double.parseDouble(datos[5])));
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return guantes;
     }
 
-    public List<Pechera> leerPecheras (){
+    public List<Pechera> leerPecheras() {
         List<Pechera> pecheras = new LinkedList<>();
         try {
-            BufferedReader buffer = new BufferedReader(new FileReader("src/main/java/com/sia/tp3/testdata/pecheras.tsv"));
+            BufferedReader buffer = new BufferedReader(new FileReader(path + "pecheras.tsv"));
             String line;
             String[] datos;
             buffer.readLine();
 
-            while ((line = buffer.readLine()) != null){
+            while ((line = buffer.readLine()) != null) {
 
                 datos = line.split("\t");
-                pecheras.add(Integer.parseInt(datos[0]),  new Pechera(Double.parseDouble(datos[1]),Double.parseDouble(datos[2]),Double.parseDouble(datos[3]),Double.parseDouble(datos[4]),Double.parseDouble(datos[5])));
+                pecheras.add(Integer.parseInt(datos[0]), new Pechera(Double.parseDouble(datos[1]),
+                        Double.parseDouble(datos[2]), Double.parseDouble(datos[3]), Double.parseDouble(datos[4]),
+                        Double.parseDouble(datos[5])));
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
